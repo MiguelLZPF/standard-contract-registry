27b043569ac1a748f49a73e57bb8dddae9a2dcdb
MiguelLZPF <MiguelLZPF>
2020-11-12 20:44:56 +0100

    fixes and normalization beetween projects

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,26 +1,33 @@
-import { task } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
-import { HardhatUserConfig } from "hardhat/types";
+import "@nomiclabs/hardhat-solhint";
+import "hardhat-contract-sizer";
 
-// This is a sample Buidler task. To learn how to create your own go to
-// https://buidler.dev/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, bre) => {
-  const accounts = await bre.ethers.getSigners();
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(await account.getAddress());
+    console.log(account.address);
   }
 });
 
-const config:HardhatUserConfig = {
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+export default {
   solidity: {
     version: "0.7.4",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
-      },
-    },
+        runs: 100
+      }
+    }
   },
   defaultNetwork: "hardhat",
   networks: {
@@ -37,8 +44,11 @@ const config:HardhatUserConfig = {
     },
   },
   mocha: {
-    timeout: 999999,
+    timeout: "none"
   },
-};
-
-export default config;
+  contractSizer:{
+    alphaSort: false,
+    runOnCompile: true,
+    disambiguatePaths: false
+  }
+} as HardhatUserConfig;
