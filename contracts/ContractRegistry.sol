b8c7d0d325f23a4aac75afbcd0fb02dfa7ac0ff4
MiguelLZPF <mgcdreamer@gmail.com>
2020-11-21 18:01:46 +0100

    Update all types and versions part of the contract

--- contracts/ContractRegistry.sol
+++ contracts/ContractRegistry.sol
@@ -21,10 +21,8 @@ contract ContractRegistry is Ownable {
   // EVENTS
   event Initialized(address indexed registry, address indexed owner);
   event NewType(bytes32 indexed id, string type_, bytes2 indexed version);
-  event VersionUpdated(
-    ContractType indexed type_,
-    bytes2 indexed oldVersion
-  );
+  event VersionUpdated(ContractType indexed type_, bytes2 indexed oldVersion);
+  event TypeDeleted(ContractType indexed type_);
   event Deployed(
     address proxy,
     address logic,
@@ -59,7 +57,7 @@ contract ContractRegistry is Ownable {
     bytes2 version;
   }
   struct ContractRecord {
-    address proxy;
+    address proxy; // use as ID too
     address logic;
     address owner;
     string type_;
@@ -67,103 +65,159 @@ contract ContractRegistry is Ownable {
     uint256 dateCreated;
     uint256 dateUpdated;
   }
-
+  // ==========
   // PROPERTIES
-  // Types and versions
+  // 1. Types and versions
+  // 1.1 Array to store all known types
+  //     need an index to get/update/remove one item by reference (id)
+  //     0 index reserved for non init. 1,2,3... N
+  //     is like ask "where are this id stored?"
   ContractType[] knownTypes;
+  mapping(bytes32 => uint256) typesIndex;
+  // 1.2 Check if string is a type
   mapping(string => bool) public isType;
-  mapping(bytes32 => ContractType) public typeById;
-  // Contracts Registry
+  // DEPRECATED 1.3 Get a type by reference (id)
+  //mapping(bytes32 => ContractType) public typeById;
+  // 2. Contract Registry
+  // 2.1 Array to store all contract records
+  //     need an index to remove one item by reference (proxy address)
+  //     0 index reserved for non init. 1,2,3... N
   ContractRecord[] records;
-  mapping(address => ContractRecord) recordByProxy;
+  mapping(address => uint256) recordsIndex;
+  // DEPRECATED 2.2 Get one record by reference (proxy address)
+  //mapping(address => ContractRecord) recordByProxy;
+  // 2.2 Get all records of one owner
   mapping(address => ContractRecord[]) recordsByOwner;
-  mapping(string => ContractRecord[]) recordsByType;
+  // 2.3 Get all records of one type id
+  mapping(bytes32 => ContractRecord[]) recordsByType;
 
-  // Proxy admin contract
+  // 3. Proxy admin contract
+  //    the proxy admin contract thats use to deploy and upgrade records
   ProxyAdmin proxyAdm;
 
-  // ---------------------
+  // =========
   // FUNCTIONS
+  /**
+    0. Initialize function that acts like a constructor
+  */
   function initialize() external initializer {
+    // initializate the owner as the msg.sender through ownable contract
     __Ownable_init();
+    // creates a proxy admin needed to deploy and upgrade contract records
     proxyAdm = new ProxyAdmin();
     emit Initialized(address(this), owner());
   }
 
-  // Proxy Admin functions ---------------
-  // -- Creates a new type of contract
-
-  // Type and versions functions ---------------
-  // -- Creates a new type of contract
+  // 1. Type and versions functions
+  /**
+    1.1 Creates a new contract type
+    @param _type the name of the type to create
+    @param _version version to set the new contract type XX.YY format
+  */
   function setType(string memory _type, bytes2 _version) public onlyOwner {
     // all type strings should be lower case
     _type = S.toLower(_type);
+    // can not be defined already
     require(!isType[_type], "this type is already defined");
-    require(
-      currentVersion[_type] < _version,
-      "new version must be greater than current one"
-    );
 
-    knownTypes.push(_type);
+    // create contrat type
+    bytes32 id = S.hash(_type);
+    ContractType newType = new ContractType(id, _type, _version);
+    // store the contract type
+    knownTypes.push(newType);
+    // -- save the index + 1 = length
+    typesIndex[id] = knownTypes.length;
     isType[_type] = true;
-    currentVersion[_type] = _version;
-    emit NewType(_type, _version);
+
+    emit NewType(id, _type, _version);
   }
 
-  // -- Gets a type from an index
-  function getType(uint256 _index)
-    public
-    view
-    returns (string memory _contractType)
-  {
-    return knownTypes[_index];
+  /**
+    1.2 Gets a type by reference (id)
+    @param _id the name of the type to create
+    @return a ContractType by index
+  */
+  function getType(bytes32 _id) public view returns (ContractType) {
+    return knownTypes[typesIndex[_id] - 1];
   }
 
-  // -- Gets all types defined
-  function getTypes() public view returns (string[] memory _contractTypes) {
+  /**
+    1.2 Gets a type from a type name
+    @param _typeName the name of the type to create
+    @return a ContractType from the hashed type name
+  */
+  function getType(string memory _typeName) public view returns (ContractType) {
+    bytes32 id = S.hash(_typeName);
+    return knownTypes[typesIndex[id] - 1];
+  }
+
+  /**
+    1.3 Gets all types defined
+    @return all ContractTypes
+  */
+  function getTypes() public view returns (ContractType[]) {
     return knownTypes;
   }
 
-  // -- Checks if a type exists or not
-  function checkType(string memory _type)
-    public
-    view
-    returns (string memory _contractType)
-  {
-    // all type strings should be lower case
-    _type = S.toLower(_type);
-    if (isType[_type]) {
-      return _type;
-    } else {
-      return "generic";
-    }
-  }
-
-  // -- Sets the latest version of a contract available
-  function setVersion(string memory _type, bytes2 _version) public onlyOwner {
-    // all type strings should be lower case
-    _type = S.toLower(_type);
+  /**
+    1.4 Sets the latest version of an available contract
+    @param _typeId the reference of the contract type to be updated
+    @param _newVersion new version for the contract type
+  */
+  function setVersion(bytes32 _typeId, bytes2 _newVersion) public onlyOwner {
     require(
-      isType[_type],
+      typesIndex[_typeId] > 0,
       "this type is not defined, use 'setType' function instead"
     );
-    bytes2 _oldVer = currentVersion[_type];
+    // get contract type from id
+    uint256 index = typesIndex[_typeId] - 1;
+    ContractType typeToUpdate = knownTypes[index];
+
     require(
-      currentVersion[_type] < _version,
+      typeToUpdate.version < _newVersion,
       "new version must be greater than current one"
     );
-
-    currentVersion[_type] = _version;
-    emit VersionUpdated(_type, _oldVer, _version);
+    emit VersionUpdated(typeToUpdate, typeToUpdate.version);
+    // set the new version to the type
+    typeToUpdate.version = _newVersion;
+    // store the new contract type
+    knownTypes[typesIndex[typeToUpdate.id] - 1] = typeToUpdate;
   }
 
-  // -- Gets the latest version avilable for a contract type
-  function getVersion(string memory _type)
-    public
-    view
-    returns (bytes2 _version)
-  {
-    return currentVersion[S.toLower(_type)];
+  /**
+    1.5 Gets the version of a contract type by reference (id)
+    @param _typeId the id of the type to create
+    @return the latest contract type version by index
+  */
+  function getVersion(bytes32 _typeId) public view returns (bytes2) {
+    return knownTypes[typesIndex[_typeId] - 1].version;
+  }
+
+  /**
+    1.6 Gets the version of a contract type from a type name
+    @param _typeName the name of the type to create
+    @return the latest contract type version from the hashed type name
+  */
+  function getVersion(string memory _typeName) public view returns (bytes2) {
+    bytes32 typeId = S.hash(_typeName);
+    return knownTypes[typesIndex[typeId] - 1].version;
+  }
+
+  /**
+    1.7 Removes a contract type from storage
+    @param _typeId the id of the contract type to remove  
+  */
+  function removeType(bytes32 _typeId) public {
+    uint256 index = typesIndex[_typeId] - 1;
+    emit TypeDeleted(knownTypes[index]);
+    // get last element of the array
+    ContractType last = knownTypes[knownTypes.length - 1];
+    // write the last element on the index of the contract type to remove
+    knownTypes[index] = last;
+    // update the index of the (old) last contract type
+    typesIndex[last.id] = index;
+    // remove duplicated last element from the array
+    knownTypes.pop();
   }
 
   // Contracts Registry Functions -------------------
